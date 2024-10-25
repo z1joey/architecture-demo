e2ad4fbf2ea05dd7642cc589f28bbbc5727709fb
JoeyZhang <z1joey@iCloud.com>
2024-10-25 17:03:30 +0800

    Init project

---
+++ RIBs/RIBs/SceneDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = LoginViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
