6e37ddf047f132ff407f9439cb6e9b9b5b711cc0
JoeyZhang <z1joey@iCloud.com>
2024-11-12 15:44:48 +0800

    Init project

---
+++ FactoryVIPER/FactoryVIPER/FactoryVIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryVIPERApp.swift
+//  FactoryVIPER
+//
+//  Created by Joey Zhang on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct FactoryVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
